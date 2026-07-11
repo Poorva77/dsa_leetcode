8682704b0fb593b486edff037ac2875bfb94ad4c
Poorva Jaiswal <poorvajaiswal2708@gmail.com>
2026-07-11 22:22:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (93.6%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()) return false;
+        for(int i=0;i<s.size();i++){
+            if(s==goal) return true;
+            char ch=s[0];
+            s.erase(s.begin());
+            s.push_back(ch);
+        }
+        return false;
+    }
+};
