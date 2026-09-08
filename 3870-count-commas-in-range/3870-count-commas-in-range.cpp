d215361335de0d4d8fa42c8ba4f4961161883f66
Poorva Jaiswal <poorvajaiswal2708@gmail.com>
2026-09-09 00:09:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -2,16 +2,6 @@ class Solution {
 public:
     int countCommas(int n) {
         if(n<1000) return 0;
-        if(n==100000) return 99001;
-        string num=to_string(n);
-        int comma=0;
-        int power=3;
-        int ans=0;
-        for(int i=2;i<num.size();i+=3){
-            comma++;
-            ans+=n-(pow(10,power))+1;
-            power+=3;
-        }
-        return ans;
+        return n-999;
     }
 };
